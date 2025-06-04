5bc540d2ed17dbd6e9fa686acda4aa146bb6fb3c
bugrahan14 <ertasbugra0611@gmail.com>
2025-06-04 16:06:18 +0300

    CRUD tamamlandı: PUT ve DELETE endpointleri eklendi, UTF-8 sorunu çözüldü

--- src/main/java/com/bugrahan/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/bugrahan/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.bugrahan.springboot.cruddemo.dao.EmployeeDAO;
 import com.bugrahan.springboot.cruddemo.entity.Employee;
 import com.bugrahan.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,7 +24,97 @@ public class EmployeeRestController {
         return employeeService.findAll();
     }
 
+    //  ADD MAPPİNG FOT GET /employees({employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee==null){
+            throw new RuntimeException("YANLIŞ İD -- EMPLOYEE İD NOT FOUND " + employeeId);
+
+        }
+        return theEmployee;
     }
+
+
+     // ADD MAPPİNG FOR "POST" /employess - add new employee
+    // @PostMapping
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+        // ALSO JUST İN CASE THEY PASS AN İD İN JSON .... SET İD TO 0
+
+        // THİS İS TO FORCE A SAVE OF NEW İTEM ..... İNSTEAD FOR UPDATE
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return  dbEmployee;
+
+        //return employeeService.save(theEmployee);
+    }
+
+
+    // ADD MAPPİNG FOR PUT /employees - UPDATE EXİSTİNG EMPLOYEE
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        // Check if there is an employee with this ID in the database
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee ID bulunamadı: " + employeeId);
+        }
+
+        // delete
+        employeeService.deleteById(employeeId);
+
+        return "Silinen çalışanın ID'si(ID of deleted employee): " + employeeId;
+    }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
